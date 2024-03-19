be72e263e43b12da2a049b5ae45a695a5e310ad1
toutouzi <1300536186@qq.com>
2024-03-19 10:03:46 +0800

    finish lab2

--- src/tcp_receiver.cc
+++ src/tcp_receiver.cc
@@ -5,11 +5,39 @@ using namespace std;
 void TCPReceiver::receive( TCPSenderMessage message )
 {
   // Your code here.
-  (void)message;
+  // ISN_.has_value()功能是+1， 不直接+1的是因为第一个报文为0；
+  const uint64_t checkpoint = reassembler_.writer().bytes_pushed() + ISN_.has_value();
+  if ( message.SYN ) {
+    if ( !ISN_.has_value() ) {
+      ISN_ = message.seqno;
+    }
+  } else if ( message.seqno == ISN_ ) {
+    return;
+  }
+
+  if ( message.RST ) {
+    TCPReceiver::reader().set_error(); // 不能调用writer(),因为writer()没有non_const的函数，set_error是non_error
+  }
+
+  const uint64_t abseqno = message.seqno.unwrap( *ISN_, checkpoint );
+  TCPReceiver::reassembler_.insert( abseqno == 0 ? abseqno : abseqno - 1, move( message.payload ), message.FIN );
 }
 
 TCPReceiverMessage TCPReceiver::send() const
 {
   // Your code here.
-  return {};
+  TCPReceiverMessage rev_message {};
+  if ( ISN_.has_value() ) {
+    rev_message.ackno = Wrap32::wrap(
+      reassembler_.writer().bytes_pushed() + ISN_.has_value() + reassembler_.writer().is_closed(), *ISN_ );
+  }
+
+  uint64_t capacity = reassembler_.writer().available_capacity();
+  rev_message.window_size = capacity > UINT16_MAX ? UINT16_MAX : capacity;
+
+  if ( TCPReceiver::writer().has_error() ) {
+    rev_message.RST = true;
+  }
+
+  return rev_message;
 }
--- src/wrapping_integers.cc
+++ src/wrapping_integers.cc
@@ -5,15 +5,19 @@ using namespace std;
 Wrap32 Wrap32::wrap( uint64_t n, Wrap32 zero_point )
 {
   // Your code here.
-  (void)n;
-  (void)zero_point;
-  return Wrap32 { 0 };
+  return Wrap32( zero_point + static_cast<uint32_t>( n ) );
 }
 
 uint64_t Wrap32::unwrap( Wrap32 zero_point, uint64_t checkpoint ) const
 {
   // Your code here.
-  (void)zero_point;
-  (void)checkpoint;
-  return {};
+  const constexpr uint64_t Upper_32 = 1l << 32;
+  const constexpr uint64_t half_32 = 1l << 31;
+  const uint32_t ckpt = wrap( checkpoint, zero_point ).raw_value_;
+  uint32_t offset = raw_value_ - ckpt;
+
+  if ( offset < half_32 || checkpoint + offset < Upper_32 ) {
+    return checkpoint + offset;
+  }
+  return checkpoint + offset - Upper_32;
 }
